93ad121229564d2ab3e050594895d0794a96540e
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-04 00:30:39 +0300

    modified php_file/basic.php

--- php_file/basic.php
+++ php_file/basic.php
@@ -1,8 +1,26 @@
 <?php
 
-$i = 15;
-$j = 20;
-
-print($i+$j);
+$table = 11;
+$rows = 100;
+$i = 0;
 
 ?>
+<table border="2">
+    <tr>
+        <th>Multiplicant</th>
+        <th>Multiplier</th>
+        <th>Result</th>
+    </tr>
+    <?php
+    for ($i = 0; $i < $rows; $i++) {
+
+    ?>
+        <tr>
+            <td><?php print($table); ?></td>
+            <td><?php print($i); ?></td>
+            <td><?php print($i * $table); ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
