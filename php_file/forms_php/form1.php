f3410411f6f5c0fe80c27e891da2216698e7c048
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-11 00:02:10 +0300

    created forms_php/form1.php and php_server.php

---
+++ php_file/forms_php/form1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+</head>
+
+<body>
+
+    <h2>PHP Forms</h2>
+
+    <form action="welcome.php" method="post">
+        First Name: <input type="text" name="fname"><br>
+        Last Name: <input type="text" name="lname"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
