07a866f047acb2f559e8576b719ac80a0fd634f3
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-14 15:47:43 +0300

    created form3.php

---
+++ php_form/form3.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form Validation</title>
+</head>
+
+<body>
+    <?php
+    $name = $email = $website = $comment = $gender = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = input($_POST["name"]);
+        $email = input($_POST["email"]);
+        $website = input($_POST["website"]);
+        $comment = input($_POST["comment"]);
+        $gender = input($_POST["gender"]);
+    }
+    function input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
+
+
+    <h2>PHP Form Validation Example</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name"><br><br>
+        E-mail: <input type="email" name="email"><br><br>
+        Website: <input type="text" name="website"><br><br>
+        comment: <textarea name="comment" rows="5" cols="40"></textarea><br><br>
+        Gender:
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="other">Other <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+ 
+
+</body>
+
+</html>
